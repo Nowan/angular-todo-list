2a118e21b155af297abf3539fa0df4231a65360c
Mac Mini <macmini@Mac-mini-Mac.local>
2016-04-02 13:03:14 +0200

    add todo-sidebar items

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,6 +9,23 @@
   function MainController() {
     var vm = this;
     
-    vm.text="Hello World!";
+    vm.sidebarItems = [
+        {
+            title: 'Clear All',
+            iconPath: 'assets/icons/ic_delete_black_48px.svg'
+        },
+        {
+            title: 'Clear Done',
+            iconPath: 'assets/icons/ic_clear_black_48px.svg'
+        },
+        {
+            title: 'Settings',
+            iconPath: 'assets/icons/ic_settings_black_48px.svg'
+        },
+        {
+            title: 'About',
+            iconPath: 'assets/icons/ic_about_black_48px.svg'
+        }
+    ];
   }
 })();
