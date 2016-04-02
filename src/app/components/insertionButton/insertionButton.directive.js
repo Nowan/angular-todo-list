7c78ef44654cfba5f3a89bfe9ea765602b87626a
Mac Mini <macmini@Mac-mini-Mac.local>
2016-04-02 13:31:47 +0200

    add insertion-button directive

---
+++ src/app/components/insertionButton/insertionButton.directive.js
@@ -0,0 +1,31 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('todoList')
+        .directive('insertionButton', insertionButton);
+
+    //Directive.$inject = ['dependency1'];
+    function insertionButton() {
+        // Usage:
+        //
+        // Creates:
+        //
+        var directive = {
+            bindToController: true,
+            controller: 'MainController',
+            controllerAs: 'mainCtrl',
+            link: link,
+            restrict: 'E',
+            scope: {
+            },
+            templateUrl: "app/components/insertionButton/insertion-button.html",
+            css: "app/components/insertionButton/insertion-button.css"
+        };
+        return directive;
+        
+        function link(scope, element, attrs) {
+        }
+    }
+    
+})();
